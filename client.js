262f7a2307c8208e587f683d4c268abd735bdf2b
adun1 <bmccormack15@gmail.com>
2020-06-05 02:30:31 +0000

    added sending name to server upon connect

--- client.js
+++ client.js
@@ -8,10 +8,19 @@ const connect = function() {
   //interpret as utf-8
   conn.setEncoding('utf-8');
 
+  //check for successful connection
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: BRI");
+
+  });
+  
+  
   //recieive data from server
   conn.on('data', (data) => {
     console.log(data);
   });
+
 
   return conn;
 };
